23228dc9900f7dac7472fbd00e88ff9b57708cdf
shubhamlondhe1996 <shubham.londhe@vamstar.io>
2023-05-05 17:09:21 +0530

    Addded some change

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ def to_upper(name):
     return name.upper()
 
 
-def say(name):
+def say_hello(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hello, {name}')  # Press ⌘F8 to toggle the breakpoint.
 
